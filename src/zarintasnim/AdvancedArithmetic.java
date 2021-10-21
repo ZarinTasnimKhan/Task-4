64acad732bcb3e9edcb2673db16082e459ce1e5c
ZarinTasnimKhan <zarintm1221@gmail.com>
2021-10-21 12:12:23 +0800

    first commit

---
+++ src/zarintasnim/AdvancedArithmetic.java
@@ -0,0 +1,17 @@
+/*
+Name: Zarin Tasnim Khan
+Id: 2012020178
+Section: 53(D)
+Email: cse_2012020161@lus.ac.bd
+Date: 16-10-2021
+ */
+
+
+package zarintasnim;
+
+import java.math.BigInteger;
+
+public interface AdvancedArithmetic {
+    int divisorSum(int n);
+    BigInteger findFactorial(int N);
+}
